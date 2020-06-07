63cada093e9c8416f91634af9c51025f7251aeca
Alper Deniz <alper.deniz@gazi.edu.tr>
2020-06-07 18:52:08 +0300

    Hiding was added without sound.

--- VideoSteganography/MainWindow.xaml.cs
+++ VideoSteganography/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Accord.Video.FFMPEG;
+﻿using Accord.Math;
+using Accord.Video.FFMPEG;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -9,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -24,11 +26,20 @@ namespace VideoSteganography
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region declare variables
         private string filePath = "";
         private int width, height;
-        private long framecount, capacityHiding;
+        private long framecount, capacityHiding, capacityPerFrame;
         private List<Bitmap> bitmaps = new List<Bitmap>();
+        private List<Bitmap> bitmapsEmbedded = new List<Bitmap>();
         private bool isEnded = false;
+        private string InfoHiding, InfoSolved;
+        private int countFrame, bitRate;
+        private double frameRate;
+        private VideoCodec codecName;
+        private string videoDirectory = @"C:\Users\deniz\OneDrive\Masaüstü\SCHOOL REMOTE\Stenografi\VideoSteganography\VideoSteganography\Videos";
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
@@ -68,12 +79,42 @@ namespace VideoSteganography
 
         private void BtnHide_Click(object sender, RoutedEventArgs e)
         {
+            InfoHiding = new TextRange(txtHide.Document.ContentStart, txtHide.Document.ContentEnd).Text;
+            string firstPart = String.Empty, elapsedPart = String.Empty;
+            countFrame = 0;
+            bitmapsEmbedded = bitmaps;
+            try
+            {
+                do
+                {
+                    bool isBigger = InfoHiding.Length > capacityPerFrame;
+                    firstPart = InfoHiding.Substring(0, isBigger ? (int)capacityPerFrame : InfoHiding.Length);
+                    InfoHiding = isBigger ? InfoHiding.Substring((int)capacityPerFrame, InfoHiding.Length - (int)capacityPerFrame) : String.Empty;
+                    bitmapsEmbedded[countFrame] = (Steganography.embedText(firstPart, bitmaps[countFrame]));
+                    countFrame++;
+                } while (!String.IsNullOrEmpty(InfoHiding));
+                txtStatus.Text = "Congrulations! You concealed your information successfully!";
+                createVideo(bitmapsEmbedded);
 
+            }
+            catch (Exception)
+            {
+
+                txtStatus.Text = "Something is wrong! You didn't conceal your information successfully!";
+            }
         }
 
         private void BtnSolve_Click(object sender, RoutedEventArgs e)
         {
-
+            InfoSolved = String.Empty;
+            foreach (var bitmap in bitmapsEmbedded)
+            {
+                InfoSolved += Steganography.extractText(bitmap);
+            }
+            txtStatus.Text = $"Your concealed information is solved successfully!";
+            txtSolved.IsEnabled = true;
+            txtSolved.Document.Blocks.Clear();
+            txtSolved.Document.Blocks.Add(new Paragraph(new Run(InfoSolved)));
         }
 
         private void BtnSelectVideo_Click(object sender, RoutedEventArgs e)
@@ -106,26 +147,25 @@ namespace VideoSteganography
             VideoFileReader reader = new VideoFileReader();
             reader.Open(filePath);
             framecount = reader.FrameCount;
-
+            frameRate = reader.FrameRate.Value;
+            bitRate = reader.BitRate;
+            codecName = (VideoCodec)Enum.Parse(typeof(VideoCodec), reader.CodecName.ToUpper());
             for (int i = 0; i < framecount; i++)
             {
                 Bitmap eachFrame = reader.ReadVideoFrame();
                 bitmaps.Add(eachFrame);
-                //videoFrame.Save(Environment.CurrentDirectory + "\\" + i + ".bmp");
-                // dispose the frame when it is no longer required
-                //videoFrame.Dispose();
             }
 
-            width = bitmaps[0].Width; // Width value is defining.
-            height = bitmaps[0].Height; // Height value is defining.
-            capacityHiding = framecount * width * height * 3;
-       
+            width = reader.Width; // Width value is defining.
+            height = reader.Height; // Height value is defining.
+            capacityHiding = (framecount * width * height * 3) / 8;
+            capacityPerFrame = capacityHiding / framecount;
         }
 
         private void FillTextBoxes(Microsoft.Win32.OpenFileDialog dialog)
         {
             filePath = txtVideoPath.Text = dialog.FileName;
-            selectedVideo.Source = new Uri($@"{filePath}");
+            changeVideoSource(selectedVideo, filePath);
             selectedVideo.LoadedBehavior = MediaState.Manual;
             txtName.Text = dialog.SafeFileNames[0];
             FetchPixels();
@@ -143,6 +183,26 @@ namespace VideoSteganography
             FieldInfo stateField = helperObject.GetType().GetField("_currentState", BindingFlags.NonPublic | BindingFlags.Instance);
             MediaState state = (MediaState)stateField.GetValue(helperObject);
             return state;
+        }
+
+        private void createVideo(List<Bitmap> imageList)
+        {
+            using (var writer = new VideoFileWriter())          // Initialize a VideoFileWriter
+            {
+                string path = $@"{videoDirectory}\{Guid.NewGuid()}.mp4";
+                writer.Open(path, imageList[0].Width, imageList[0].Height, (int)frameRate, VideoCodec.MPEG4, bitRate);              // Start output of video
+                foreach (var bmp in imageList)
+                {
+                    writer.WriteVideoFrame(bmp);
+                }
+                writer.Close();                                 // Close VideoFileWriter
+                changeVideoSource(newVideo, path);
+            }
+        }
+
+        private void changeVideoSource(MediaElement mediaElement, string url)
+        {
+            mediaElement.Source = new Uri(url);
         }
     }
 }
